57c08029366685bcc0d99cb4d54b6b1b98b8435b
rizak <rizak.ahmed@gmail.com>
2017-09-01 14:56:21 +0200

    Update groovy script with last version of google analytics Universal tracking code

--- src/main/resources/WEB-INF/scripts/googleAnalytics.groovy
+++ src/main/resources/WEB-INF/scripts/googleAnalytics.groovy
@@ -1,16 +1,10 @@
-println("<script type=\"text/javascript\">\n" +
-                    "\n" +
-                    "  var _gaq = _gaq || [];\n");
-
-println(" _gaq.push(['_setAccount', '${webPropertyID}']);\n" +
-                    "  _gaq.push(['_trackPageview', '${resourceUrl}']);\n")
-gaMap.each {
-  entry-> println("  _gaq.push(['_trackPageview', '${entry.value}']);\n")
-}
-println("  (function() {\n" +
-                    "    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n" +
-                    "    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n" +
-                    "    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n" +
-                    "  })();\n" +
-                    "\n" +
-                    "</script>")
+println("<script>\n" +
+        "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n" +
+        "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n" +
+        "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n" +
+        "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\n" +
+        "\n" +
+        "ga('create', '${webPropertyID}', 'auto');\n" +
+        "ga('send', 'pageview');\n" +
+        "\n" +
+        "</script>" )
